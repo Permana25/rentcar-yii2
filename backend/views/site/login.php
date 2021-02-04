e74ffc77312dab0298a00ad8b76e990b9c8eabc9
Permana25 <agaschapoetra@gmail.com>
2021-02-04 10:24:47 +0700

    merubah tampilan login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -13,23 +13,28 @@ $this->title = 'Sign In';
 
 $fieldOptions1 = [
     'options' => ['class' => 'form-group has-feedback'],
-    'inputTemplate' => "{input}<span class='glyphicon glyphicon-envelope form-control-feedback'></span>"
+    'inputTemplate' => "{input}<span style='color: #000000;' class='glyphicon glyphicon-envelope form-control-feedback'></span>"
 ];
 
 $fieldOptions2 = [
     'options' => ['class' => 'form-group has-feedback'],
-    'inputTemplate' => "{input}<span class='glyphicon glyphicon-lock form-control-feedback'></span>"
+    'inputTemplate' => "{input}<span style='color: #000000;' class='glyphicon glyphicon-lock form-control-feedback'></span>"
 ];
 ?>
 
 <div class="login-box">
     <div class="login-logo">
         </div>
-    <!-- /.login-logo -->
-
-    <div class="login-box-body" style="background:rgba(255,255,255, 0.1)">
-
-        <img src="upload/logo1.png" width="320"><br><br>
+            <div class="login-box-body" style="background:rgba(255,255,255, 0.1)">
+            <div align="center">
+                <div class="row">
+                    <div class="col-md-12 formnya" style="margin-left:0px;">
+                        <div class="row" style="left: auto; margin: 10px;">
+                            <div class="col-md-1"></div>
+                                <div class="col-md-10" style="margin-top: 0px; margin-bottom: 10px; color: black;">
+                                    <p>WELCOME TO</p>
+                                        <h2 style="color: black; margin-top: 10px;" class="gss"><b>GSS RENTAL</b> 
+                                            <img src="upload/logo1.png" width="200px"><br><br></h2>
 
 
         <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
@@ -43,14 +48,23 @@ $fieldOptions2 = [
             ->field($model, 'password', $fieldOptions2)
             ->label(false)
             ->passwordInput(['placeholder' => $model->getAttributeLabel('password')]) ?>
+   
 
-        <div class="row">
-            <!-- /.col -->
-            <div class="col-xs-4">
-                <?= Html::submitButton('Sign in', ['class' => 'btn btn-primary btn-block btn-flat', 'name' => 'login-button']) ?>
-            </div>
-            <!-- /.col -->
+        <div class="form-group">
+            <?= Html::submitButton('LOGIN', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
         </div>
+        <div class="col-md-1"></div>
+        <br>
+
+        <div class="row" style="margin-top: -50px;">
+                        <div class="col-lg-12 info">
+                            <p>About Developer</p>
+                            <a href="https://www.facebook.com/garudasaranasejahtera"><i class="fa fa-facebook"></i></a> &nbsp;
+                            <a href="https://www.instagram.com/gss_software/"><i class="fa fa-instagram"></i></a> &nbsp;
+                            <a href="http://klikgss.com"><i class="fa fa-globe"></i></a> &nbsp;
+                            <a href="mailto:informasi@klikgss.com"><i class="fa fa-envelope"></i></a> &nbsp;
+                        </div>
+                    </div>
 
 
         <?php ActiveForm::end(); ?>
@@ -59,3 +73,10 @@ $fieldOptions2 = [
     </div>
     <!-- /.login-box-body -->
 </div><!-- /.login-box -->
+<style>
+.info a {
+        /* color: rgba(22, 194, 230, 1); */
+        color: black;
+    }
+
+</style>
