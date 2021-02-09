1e405837d6fa6ed0f7d279afcc0d853927454a7a
Permana25 <agaschapoetra@gmail.com>
2021-02-09 09:13:14 +0700

    update database

--- rental2.sql
+++ rental2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Feb 2021 pada 03.00
+-- Waktu pembuatan: 09 Feb 2021 pada 03.11
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 7.3.3
 
